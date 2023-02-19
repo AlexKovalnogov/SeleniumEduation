1753788d4bf7fc34df83a493d50defd16053c222
Oleksii <kovalnogovalex@gmail.com>
2023-02-19 18:56:09 +0200

    first commit

---
+++ src/test/java/Executor/TestRunner.java
@@ -0,0 +1,16 @@
+package Executor;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestRunner {
+
+
+    @Before
+public void beforeTest(){}
+
+
+
+    @After
+    public void afterTest(){}
+}
